1eb5b36f27628837bec9c37ac2c233743dc00dba
Shota nishiyama <n.i.s.h.i.4869@gmail.com>
2021-09-06 23:13:57 +0900

    MapのViewとdelegateの修正を反映

--- Medicus/AppDelegate.swift
+++ Medicus/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import FirebaseUI
+import GoogleMaps
 
 
 @UIApplicationMain
@@ -35,8 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        //FirebaseApp.configure()
+        GMSServices.provideAPIKey(GoogleMapAccessToken)
         return true
     }
 
---
+++ Medicus/DoctorMapViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DoctorMapViewController.swift
+//  Medicus
+//
+//  Created by 西山翔太 on 2021/09/03.
+//  Copyright © 2021 西山翔太. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class DoctorMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
